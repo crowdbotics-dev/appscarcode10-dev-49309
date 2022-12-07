9be897fc69275c13eb57178473325ee51ba2098e
scartag <tag@crowdbotics.com>
2022-12-07 18:31:43 +0000

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -38,7 +38,7 @@ export const globalOptions = {
 };
 export const modulesOptions = {
   "@modules/app-menu": {
-    "title": "App Menu67",
+    "title": "App Menu7",
     "copy": "Routes available!",
     "styles": styles
   }
